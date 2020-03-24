cd92476be3cacc154e049f9ba1e3e7afc700bad8
Brian <briansterling93@yahoo.com>
2020-03-23 19:08:01 -0600

    styling

--- client/src/components/AddJob.js
+++ client/src/components/AddJob.js
@@ -7,10 +7,31 @@ const AddJob = () => {
     <div id="post-main">
       <div id="post-secondary">
         <div id="form-main">
-          <h1>Add a New Opporunity Below</h1>
-          <form-secondary>
-            <input />
-          </form-secondary>
+          <div id="form-secondary">
+            <div className="form-input">
+              <h2>Job Title</h2>
+              <input placeholder="Job Title" />
+            </div>
+            <div className="form-input">
+              <h2>Technologies Required</h2>
+              <input placeholder="Python, C#, Java, React, etc.." />
+            </div>
+            <div className="form-input">
+              <h2>Salary</h2>
+              <input placeholder="Annual Salary" />
+            </div>
+            <div className="form-input">
+              <h2>Job Description</h2>
+              <input placeholder="Job Description" />
+            </div>
+            <div className="form-input">
+              <h2>Contact Email</h2>
+              <input placeholder="Contact Email Address" />
+            </div>
+            <div className="form-input">
+              <button>Submit</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
