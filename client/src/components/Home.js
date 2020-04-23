fda4482e6a6928658066575595b04214d74caffe
Brian <briansterling93@yahoo.com>
2020-04-23 08:48:23 -0600

    styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -76,6 +76,40 @@ const Home = () => {
       console.log(error);
     }
   };
+
+  //method to handle onChange input & query job results
+  const handleSearch = (e) => {
+    dispatch({ type: "SEARCH_JOB", payload: e.target.value });
+    handleQuery();
+  };
+
+  //error handling if no queries found
+  const handleError = async () => {
+    try {
+      if (state.queriedJob.length < 1) {
+        await dispatch({
+          type: "QUERY_JOB",
+          payload: (
+            <div id="query-error-div">
+              <div id="query-error">
+                <div id="query-face">:(</div>
+                <div>
+                  <p>
+                    No current roles are available yet regarding your search,
+                    feel free to check back later or search for another
+                    technology
+                  </p>
+                </div>
+              </div>
+            </div>
+          ),
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div id="home-main">
       <div id="home-secondary">
@@ -86,19 +120,14 @@ const Home = () => {
         <div id="home-query">
           <div id="home-search">
             <input
-              // onChange={(e) => setQuery(e.target.value)}
-              onChange={(e) =>
-                dispatch({ type: "SEARCH_JOB", payload: e.target.value })
-              }
+              onChange={handleSearch}
               placeholder="Enter a technology (React, Node, Etc..)"
             />
             <div id="btn-div">
-              <button onClick={handleQuery} id="home-btn">
-                Search
-              </button>
-
               <NavLink to="/jobs/queried">
-                <button>Test</button>
+                <button onClick={handleError} id="home-btn">
+                  Search
+                </button>
               </NavLink>
             </div>
           </div>
