56ded429b51742b0625ae6021a21ee5c9d9970d2
Brian <briansterling93@yahoo.com>
2020-04-26 04:05:35 -0600

    styling

--- client/src/components/AddJob.js
+++ client/src/components/AddJob.js
@@ -92,72 +92,77 @@ const AddJob = () => {
 
   return (
     <div id="post-main">
-      <div id="post-secondary">
-        <div id="form-main">
-          <div id="form-secondary">
-            <div className="form-input">
-              <h2>Job Title</h2>
-              <input
-                value={state.title}
-                onChange={(e) =>
-                  dispatch({ type: "UPDATE_TITLE", payload: e.target.value })
-                }
-                placeholder="Job Title"
-              />
-              <div id="error-div">{titleError}</div>
-            </div>
-            <div className="form-input">
-              <h2>Technologies Required</h2>
-              <input
-                value={state.tech_stack}
-                onChange={(e) =>
-                  dispatch({ type: "UPDATE_TECH", payload: e.target.value })
-                }
-                placeholder="Python, C#, Java, React, etc.."
-              />
-              <div id="error-div">{techError}</div>
-            </div>
-            <div className="form-input">
-              <h2>Salary</h2>
-              <input
-                value={state.salary}
-                onChange={(e) =>
-                  dispatch({ type: "UPDATE_SALARY", payload: e.target.value })
-                }
-                placeholder="Annual Salary"
-              />
-              <div id="error-div">{salaryError}</div>
-            </div>
-            <div className="form-input">
-              <h2>Job Description</h2>
+      <div id="form-padding">
+        <div id="post-secondary">
+          <div id="form-main">
+            <div id="form-secondary">
+              <div className="form-input">
+                <h2>Job Title</h2>
+                <input
+                  value={state.title}
+                  onChange={(e) =>
+                    dispatch({ type: "UPDATE_TITLE", payload: e.target.value })
+                  }
+                  placeholder="Job Title"
+                />
+                <div id="error-div">{titleError}</div>
+              </div>
+              <div className="form-input">
+                <h2>Technologies Required</h2>
+                <input
+                  value={state.tech_stack}
+                  onChange={(e) =>
+                    dispatch({ type: "UPDATE_TECH", payload: e.target.value })
+                  }
+                  placeholder="Python, C#, Java, React, etc.."
+                />
+                <div id="error-div">{techError}</div>
+              </div>
+              <div className="form-input">
+                <h2>Salary</h2>
+                <input
+                  value={state.salary}
+                  onChange={(e) =>
+                    dispatch({ type: "UPDATE_SALARY", payload: e.target.value })
+                  }
+                  placeholder="Annual Salary"
+                />
+                <div id="error-div">{salaryError}</div>
+              </div>
+              <div className="form-input">
+                <h2>Job Description</h2>
 
-              <textarea
-                rows="3"
-                cols="50"
-                value={state.descrip}
-                onChange={(e) =>
-                  dispatch({
-                    type: "UPDATE_DESCRIPTION",
-                    payload: e.target.value,
-                  })
-                }
-                placeholder="Describe The Details Of This Position"
-              ></textarea>
-              <div id="error-div">{descripError}</div>
-            </div>
-            <div className="form-input">
-              <h2>Contact Email</h2>
-              <input
-                value={state.contact}
-                onChange={(e) =>
-                  dispatch({ type: "UPDATE_CONTACT", payload: e.target.value })
-                }
-                placeholder="Contact Email Address"
-              />
-              <div id="error-div">{contactError}</div>
-            </div>
-            <div className="form-input">
-              <button onClick={onSubmit}>Post Job</button>
+                <textarea
+                  rows="3"
+                  cols="50"
+                  value={state.descrip}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "UPDATE_DESCRIPTION",
+                      payload: e.target.value,
+                    })
+                  }
+                  placeholder="Describe The Details Of This Position"
+                ></textarea>
+                <div id="error-div">{descripError}</div>
+              </div>
+              <div className="form-input">
+                <h2>Contact Email</h2>
+                <input
+                  value={state.contact}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "UPDATE_CONTACT",
+                      payload: e.target.value,
+                    })
+                  }
+                  placeholder="Contact Email Address"
+                />
+                <div id="error-div">{contactError}</div>
+              </div>
+              <div className="form-input">
+                <button onClick={onSubmit}>Post Job</button>
+              </div>
             </div>
           </div>
         </div>
